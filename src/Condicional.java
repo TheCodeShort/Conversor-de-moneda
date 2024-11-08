a76d92c946147c786e0c0c969cde00d29bcdf2b9
ranndy <ranndy180@gmail.com>
2024-11-07 21:35:39 -0500

    actualizacion error de divisa mal escrita , falta error numero cantidad falla

--- src/Condicional.java
+++ src/Condicional.java
@@ -2,12 +2,7 @@ public class Condicional {
 	
 	public void condicion( String monedaBase, String monedaCotizada, double miMonedaLimpia, int salir){
 
-		if(monedaBase instanceof String && monedaCotizada instanceof String && miMonedaLimpia == miMonedaLimpia){
-			salir = 1;
-			
-		}else {
-			System.out.println("Los tipos de datos que digitasre estan mal");
-		}
+
 
 
 	}
--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -14,8 +14,7 @@ public class Principal {
 
         int nun = 0;
         while (nun != 1) {
-            int salir = 0;
-            while (salir != 1) {
+
                 Scanner monedas = new Scanner(System.in);
                 System.out.print("Digita la moneda base: ");
                 String monedaBase = monedas.nextLine().trim().toUpperCase();
@@ -24,54 +23,64 @@ public class Principal {
                 System.out.print("Digita la cantidad: ");
                 String miMoneda = monedas.nextLine().trim();
                 double miMonedaLimpia = Integer.parseInt(miMoneda);
+                if (miMonedaLimpia > 0){
+
+                }
                 System.out.println("************************************************");
 
 
+            if( monedaBase.matches("[a-zA-Z]+") && monedaCotizada.matches("[a-zA-Z]+")) {
+
+
+                URI direccion = URI.create("https://v6.exchangerate-api.com/v6/c9ec240d260c224f73f1cdd9/pair/" + monedaBase + "/" + monedaCotizada);
+
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(direccion)
+                        .build();
+
+
+                try {
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    /*System.out.println(response.body());  // Aquí imprime el contenido de la respuesta*/
+
+                    Monedas moneda = new Gson().fromJson(response.body(), Monedas.class);//se convierte a un objeto para java
+                    Calculos calculosAPI = new Calculos(moneda);
+
+                    var calculos = new Calculos(monedaBase, monedaCotizada, calculosAPI.calculoMoneda(miMonedaLimpia));
+                    System.out.println(calculos);
+                    System.out.println("***************************************");
+
+
+                } catch (IOException | InterruptedException e) {
+                    throw new RuntimeException(e);
+
+                } catch (NumberFormatException e){
+                    System.out.println("El numero que digito no es valido vuelve a intentarlo");
+
+                }catch (Exception e) {
+                    System.out.println("Ocurrio un error inesperado vuelve a intentarlo, " + e.getMessage());
+                }
+                System.out.print("Quieres saber de otra moneda \n1) si / 2) no:");
+
+                int opcion  = monedas.nextInt();
+
+                switch (opcion){
+                    case 1:
+                        System.out.println("\nok sigamos");
+                        break;
+                    default:
+                        System.out.println("Gracias por utilizar nuestra App");
+                        nun = 1;
+                        break;
+                }
+
+
+            }else {
+                System.out.println("Los tipos de datos que digito estan mal");
             }
 
 
-            URI direccion = URI.create("https://v6.exchangerate-api.com/v6/c9ec240d260c224f73f1cdd9/pair/" + monedaBase + "/" + monedaCotizada);
-
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(direccion)
-                    .build();
-
-
-            try {
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                /*System.out.println(response.body());  // Aquí imprime el contenido de la respuesta*/
-
-                Monedas moneda = new Gson().fromJson(response.body(), Monedas.class);//se convierte a un objeto para java
-                Calculos calculosAPI = new Calculos(moneda);
-
-                var calculos = new Calculos(monedaBase, monedaCotizada, calculosAPI.calculoMoneda(miMonedaLimpia));
-                System.out.println(calculos);
-                System.out.println("***************************************");
-
-
-            } catch (IOException | InterruptedException e) {
-                throw new RuntimeException(e);
-
-            } catch (NumberFormatException e){
-                System.out.println("El numero que digito no es valido vuelve a intentarlo");
-
-            }catch (Exception e) {
-                System.out.println("Ocurrio un error inesperado vuelve a intentarlo, " + e.getMessage());
-            }
-            System.out.print("Quieres saber de otra moneda \n1) si / 2) no:");
-
-            int opcion  = monedas.nextInt();
-
-            switch (opcion){
-                case 1:
-                    System.out.println("\nok sigamos");
-                    break;
-                default:
-                    System.out.println("Gracias por utilizar nuestra App");
-                    nun = 1;
-                    break;
-            }
 
         }
     }
